dbfde24fa98eeb49bcfe0682ba92d4b2a3f5ec5e
Emmanuel Egomson <fegomson@gmail.com>
2021-03-26 10:30:09 -0500

    setting up GraphQl

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
 const PORT = 4000
 const app = express()
+
+// middleware
+app.use('/graphql', graphqlHTTP({
+  
+}))
 
 app.listen(PORT, () => {
   console.log(`Now listening for requests on port ${PORT}`);
